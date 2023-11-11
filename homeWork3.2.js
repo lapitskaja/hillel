25cc4abae1587bb413739a4202990c498c537385
annalapitska <lapitskaja.anna@gmail.com>
2023-11-12 01:17:49 +0200

    ht2

---
+++ homeWork3.2.js
@@ -0,0 +1,11 @@
+let a ='2', b ='4';
+console.log((+a)+(+b));
+
+let c =7.2, d ='1.2';
+console.log(c/d);
+
+let e ='this', f =' simple', g =' string';
+console.log(e+f+g);
+
+let h  ='answer', i =' is:', j ='30', k =5;
+console.log(h+i+((+j)+k));

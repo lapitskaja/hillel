d681cd7e8efcb805181052ca510652c350687623
annalapitska <lapitskaja.anna@gmail.com>
2023-11-17 19:16:18 +0200

    Create homework4.2.js

---
+++ homework4.2.js
@@ -0,0 +1,13 @@
+let i = prompt('Enter any value');
+let bool = i.toLowerCase();
+if (isNaN(i)){
+    if (bool === 'true' || bool === 'false'){
+        console.log('You entered a boolean');
+    } else if (/[a-zа-яё]/i.test(i) && !/[0-9]/i.test(i)){
+        console.log('You entered a string');
+    } else {
+        console.log('You entered a complex data');
+    }
+}else{
+    console.log('You entered a number');
+}
